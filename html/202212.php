c71304d4162f8b3f697913cac1384061e43fb782
evepoi <evepoi@naver.com>
2024-03-26 18:10:22 +0900

    20221213 사진수정

--- html/202212.php
+++ html/202212.php
@@ -10,6 +10,27 @@
                     <?php include ("Year.php"); ?>
                     <?php include ("Month_2022.php"); ?>
                     <div class="Day_infor clearfix">
+                        <h2>12월 13일 수호천사어린이집</h2>    
+                        <div>
+                            <div class="photolist_img clearfix">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121301.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121302.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121303.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121304.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121305.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121306.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121307.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121308.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121309.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121310.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121311.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121312.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121313.jpg?raw=true" alt="">
+                                <img src="https://github.com/evepoi/minenphoto/blob/minengallery/2022/12/13/2022121314.jpg?raw=true" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="Day_infor clearfix">
                         <h2>12월 12일 수호천사어린이집</h2>    
                         <div>
                             <div class="photolist_img clearfix">
